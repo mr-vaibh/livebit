e70be2e899f95f18397c4edb8626c80784a428ad
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2020-01-09 21:04:10 +0530

    changed DB.
    changed SQLITE database to MYSQL database (db4free.net)

--- livebit/settings.py
+++ livebit/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'livebit.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'livebit',
+		'USERNAME': 'mrvaibh',
+		'PASSWORD': 'vaibhav1928',
+		'HOST': 'db4free.net',
 	}
+
+	# 'default': {
+	# 	'ENGINE': 'django.db.backends.sqlite3',
+	# 	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	# }
 }
 
 

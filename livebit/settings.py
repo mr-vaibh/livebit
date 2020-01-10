7ab96bbfc965f7c25e75318fe67e230ffc140f3c
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2020-01-10 22:12:26 +0530

    Update settings.py

--- livebit/settings.py
+++ livebit/settings.py
@@ -76,19 +76,19 @@ WSGI_APPLICATION = 'livebit.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-	# 'default': {
-	# 	'ENGINE': 'django.db.backends.mysql',
-	# 	'NAME': 'livebit',
-	# 	'USER': 'mrvaibh',
-	# 	'PASSWORD': 'vaibhav1928',
-	# 	'HOST': 'db4free.net',
-	# 	'PORT': '3306',
-	# }
-
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'livebit',
+		'USER': 'mrvaibh',
+		'PASSWORD': 'vaibhav1928',
+		'HOST': 'db4free.net',
+		'PORT': '3306',
 	}
+
+	# 'default': {
+	# 	'ENGINE': 'django.db.backends.sqlite3',
+	# 	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	# }
 }
 
 
